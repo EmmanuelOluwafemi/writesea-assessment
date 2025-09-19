581ecdcb6ee1691a91283e0adde284adb964ecca
EmmanuelOluwafemi <emaz4me@gmail.com>
2025-09-19 09:16:33 +0100

    fix: fixed image not showing issue

--- src/constants/recommended-connect.ts
+++ src/constants/recommended-connect.ts
@@ -5,28 +5,28 @@ export const recommendedConnects: RecommendedConnect[] = [
         name: "Moremi Chris",
         bio: "Product Designer | Crafting User-Centric Experiences",
         moreInfo: "April 15, 2023, Moremi was Chris’s mentor",
-        imageUrl: "/src/assets/avatar.png",
+        imageUrl: "/avatar.png",
         id: 1
     },
     {
         name: "Moremi Chris",
         bio: "Product Designer | Crafting User-Centric Experiences",
         moreInfo: "April 15, 2023, Moremi was Chris’s mentor",
-        imageUrl: "/src/assets/avatar.png",
+        imageUrl: "/avatar.png",
         id: 2
     },
     {
         name: "Moremi Chris",
         bio: "Product Designer | Crafting User-Centric Experiences",
         moreInfo: "April 15, 2023, Moremi was Chris’s mentor",
-        imageUrl: "/src/assets/avatar.png",
+        imageUrl: "/avatar.png",
         id: 3
     },
     {
         name: "Moremi Chris",
         bio: "Product Designer | Crafting User-Centric Experiences",
         moreInfo: "April 15, 2023, Moremi was Chris’s mentor",
-        imageUrl: "/src/assets/avatar.png",
+        imageUrl: "/avatar.png",
         id: 4
     },
 ]
